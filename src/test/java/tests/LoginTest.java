833efb7f2213e43ce6b9d341a16ca2f5a9208c25
Dima_Stremous <stremous225rus@gmail.com>
2023-01-24 21:10:22 +0300

    init

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -14,17 +14,6 @@ import pages.project.AddProjectPage;
 import steps.NavigationSteps;
 
 public class LoginTest extends BaseTest {
-
-    //@Test
-    public void loginTest() {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.getEmailInput().sendKeys(ReadProperties.username());
-        loginPage.getPassword().sendKeys(ReadProperties.password());
-        loginPage.getLogInButton().click();
-
-        Assert.assertTrue(new DashboardPage(driver).isPageOpened());
-    }
-
     @Test(description = "Description")
     @Issue("AQA18-12")
     @TmsLink("TC-001")
@@ -43,26 +32,15 @@ public class LoginTest extends BaseTest {
                         .isPageOpened()
         );
     }
-
-    @Test
-    public void loginIncorrectTest() {
-        Assert.assertEquals(
-                userStep.loginIncorrect(ReadProperties.username(), "sdfsdfsdf")
-                        .getErrorTextElement().getText(),
-                "Email/Login or Password is incorrect. Please try again1."
-        );
-    }
-
-    @Test
-    public void addProjectTest() {
-        Project project = new Project();
-        project.setName("WP_01");
-        userStep.loginSuccessful(ReadProperties.username(), ReadProperties.password());
-        projectSteps.addProject(project);
-
-        Assert.assertEquals(driver.findElement(By.className("page_title")).getText(),
-                project.getName());
-    }
+//
+//    @Test
+//    public void loginIncorrectTest() {
+//        Assert.assertEquals(
+//                userStep.loginIncorrect(ReadProperties.username(), "sdfsdfsdf")
+//                        .getErrorTextElement().getText(),
+//                "Email/Login or Password is incorrect. Please try again1."
+//        );
+//    }
 
     @Test
     public void radioButtonTest() {
